7cc2777e571136440b9c78ae4b56927c3d8b3e90
Nick Campion <nick.campion@fasthosts.com>
2018-05-09 11:17:00 +0100

    fix proxy

--- pts-core/objects/pts_network.php
+++ pts-core/objects/pts_network.php
@@ -241,7 +241,7 @@ class pts_network
 		// {
 			// $parameters['http']['proxy'] = 'tcp://' . $proxy_address . ':' . $proxy_port;
 			// $parameters['http']['request_fulluri'] = true;
-			$parameters['http']['proxy'] = 'tcp://100.64.0.31:3128';
+			$parameters['http']['proxy'] = 'tcp://proxy:3128';
 		//}
 
 		if(is_numeric($http_timeout) && $http_timeout > 1)
